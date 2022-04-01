66f3d453857a1056655b9bd7c3d8fd62463aa0f4
boomkit <boomkit22@gmail.com>
2022-04-01 16:48:37 +0900

    프라이머리키 두개 외래키 사용

--- club/src/main/java/com/project/club/Interest.java
+++ club/src/main/java/com/project/club/Interest.java
@@ -8,16 +8,24 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@IdClass(ItemKey.class)
 public class Interest{
 
-    @Id
-    @GeneratedValue
-    private Long id;
+    
+    //todo 
+    //id와 interest를 primary key로 하고
+    //id는 member의 id로
+//    @Id
+//    @GeneratedValue
+//    private Long id;
 
+
+    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="student_id") // foreign key 이름
     private Member member;
 
+    @Id
     private String interest;
 
 
---
+++ club/src/main/java/com/project/club/ItemKey.java
@@ -0,0 +1,9 @@
+package com.project.club;
+
+import java.io.Serializable;
+
+public class ItemKey implements Serializable
+{
+    private Long member;
+    private String interest;
+}
